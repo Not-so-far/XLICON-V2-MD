3975799491d7dea832c693b3e5ba10278ab38b74
RYUOKIIN x GRXVE <141437937+Not-so-far@users.noreply.github.com>
2024-01-08 11:05:04 +0530

    Update config.js1x

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['233533763772', 'Abraham Dwamena', true],
   ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['918402887115', '𝙂𝞒𝞦𝝯𝞢', true], 
+  ['918402887115', '𝐆 𝐫 𝐱 𝐕 𝐞  ズ', true], 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
@@ -51,9 +51,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = '𝐆 𝐫 𝐱 𝐕 𝐞  ズ'
 global.premium = 'true'
-global.packname = '𝙂𝞒𝞦𝝯𝞢𝙔𝘼𝙍𝘿' 
+global.packname = 'GRXVEYARD' 
 global.author = '@GRXVEY' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
